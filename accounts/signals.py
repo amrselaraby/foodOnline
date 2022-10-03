e1fe2fd2cf50c2ca49cde6cbb64b4cd0b239c2bf
Amr Elaraby <amrselaraby@gmail.com>
2022-10-03 06:04:08 +0200

    finished user registeration

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,15 +5,11 @@ from .models import User, UserProfile
 
 @receiver(post_save, sender=User)
 def post_save_create_profile_reciever(sender, instance, created, **kwargs):
-    print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print("user profile is created")
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
         except:
             UserProfile.objects.create(user=instance)
-            print("profile was not exist but i created one")
-        print("user is updated")
